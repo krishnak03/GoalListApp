91b22590cc9c173c601c2c5ff4596ac3064859ff
krishna <krishnaverma0522@gmail.com>
2021-08-24 21:53:38 +0530

    Working Flatlist with redereditem and its key value

--- App.js
+++ App.js
@@ -1,18 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, Bu, Button } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (enteredGoal) => {
+    setEnteredGoal(enteredGoal);
+  };
+
+  const addGoadHandler = () => {
+    setCourseGoals(currentGoals => [...currentGoals, { key: Math.random().toString(), value: enteredGoal }]);
+  };
+
   return (
-    <View style={{ padding: 50 }}>
-      <View style={{ flexDirection: 'row', justifyContent: 'space-evenly' }}>
-        <TextInput placeholder="Enter Goal" style={{ width: 200, borderColor: "black", borderWidth: 1 }} />
-        <Button title="Add" />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Enter Goal"
+          style={styles.textinput}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button
+          title="Add"
+          onPress={addGoadHandler}
+        />
       </View>
-    </View>
+      <FlatList data={courseGoals} renderItem={itemData => (
+        <View
+          style={styles.goalList}>
+          <Text >{itemData.item.value}</Text>
+        </View>)}
+      />
+    </View >
   );
 }
 
 const styles = StyleSheet.create({
-
+  screen: {
+    padding: 50
+  },
+  inputContainer: {
+    flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'
+  },
+  textinput: {
+    width: 200, borderColor: "black", borderWidth: 1
+  },
+  goalList: {
+    padding: 10, marginTop: 10, backgroundColor: '#ccc', borderColor: 'black', borderWidth: 1
+  }
 });
